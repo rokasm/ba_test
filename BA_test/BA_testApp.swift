f731d2e5aeedfd3bb73b5a3eb9e454a7e01c6c43
rokas_m <rokas1899@gmail.com>
2022-10-24 15:06:54 +0300

    Initial commit

---
+++ BA_test/BA_testApp.swift
@@ -0,0 +1,20 @@
+//
+//  BA_testApp.swift
+//  BA_test
+//
+//  Created by Rokas Mikelionis on 2022-10-24.
+//
+
+import SwiftUI
+
+@main
+struct BA_testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ BA_test/MainView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  BA_test
+//
+//  Created by Rokas Mikelionis on 2022-10-24.
+//
+
+import SwiftUI
+import CoreData
+
+struct MainView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    NavigationLink {
+                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                    } label: {
+                        Text(item.timestamp!, formatter: itemFormatter)
+                    }
+                }
+            }
+           
+        }
+    }
+
+}
+
+private let itemFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateStyle = .short
+    formatter.timeStyle = .medium
+    return formatter
+}()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+    }
+}
